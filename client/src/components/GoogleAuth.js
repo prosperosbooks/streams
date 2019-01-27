4ec903b22f9cb9cb761792d2315e11a7332fa937
Emin <allen.emin@gmail.com>
2019-01-26 23:20:09 -0500

    preliminary setup of google OAuth in browser

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+
+export default class GoogleAuth extends Component {
+  state = { isSignedIn: null };
+
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId:
+            "536587852329-gbqtct0hcp40pl2uqbn2v0sg8k3f0nfb.apps.googleusercontent.com",
+          scope: "email"
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
+    });
+  }
+
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return null;
+    } else if (this.state.isSignedIn) {
+      return (
+        <button className="ui red google button">
+          <i className="google icon" />
+          Sign out
+        </button>
+      );
+    } else {
+      return (
+        <button className="ui red google buton">
+          <i className="google icon" />
+          Sign In with Google
+        </button>
+      );
+    }
+  }
+
+  render() {
+    return <div>{this.renderAuthButton()}</div>;
+  }
+}
